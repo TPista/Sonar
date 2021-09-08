04491f2e75a315fe0109da7ffb4dd7effd9d03bf
TPista <pista.10@hotmail.com>
2021-09-08 20:09:50 -0300

    PerfilUpdate

--- screens/PerfilScreen.js
+++ screens/PerfilScreen.js
@@ -1,34 +1,202 @@
 import React, {useState, useContext} from 'react';
-import { View, Text, Button, StyleSheet, Image, TouchableOpacity} from 'react-native';
-
-import { useReducer } from 'react/cjs/react.production.min';
-
-import FormButton from '../components/FormButton';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
 
 import { AuthContext } from '../navigation/AuthProvider';
 
 
-const PerfilScreen =()  => {
-const {user, logout}=useContext (AuthContext);
-return(
-<View style={styles.containter}>
-   
-    <Text justifyContent= "center"> AJUSTES DE PERFIL DE {user.email}</Text>
-    <FormButton buttonTitle="Logout" onPress={()=> logout()} />
-    
-    
-    
-</View>
-);
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+
+import FormButton from '../components/FormButton';
+import FormButton2 from '../components/FormButton2';
+
+const PerfilScreen = () => {
+    const {user, logout}=useContext (AuthContext);
+
+  return (
+    <View style={styles.container}>
+     
+        <View style={{alignItems: 'center'}}>
+        
+          <Text style={{marginTop: 10, fontSize: 18, fontWeight: 'bold'}}>
+            {user.email}
+          </Text>
+        </View>
+
+        <View style={styles.action}>
+          <FontAwesome name="user-o" size={20} />
+          <TextInput
+            placeholder="First Name"
+            placeholderTextColor="#666666"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+              
+            ]}
+          />
+        </View>
+        <View style={styles.action}>
+          <FontAwesome name="user-o" size={20} />
+          <TextInput
+            placeholder="Last Name"
+            placeholderTextColor="#666666"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+             
+            ]}
+          />
+        </View>
+        <View style={styles.action}>
+          <Feather name="phone"  size={20} />
+          <TextInput
+            placeholder="Phone"
+            placeholderTextColor="#666666"
+            keyboardType="number-pad"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+             
+            ]}
+          />
+        </View>
+        <View style={styles.action}>
+          <FontAwesome name="envelope-o"  size={20} />
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="#666666"
+            keyboardType="email-address"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+             
+            ]}
+          />
+        </View>
+        <View style={styles.action}>
+          <FontAwesome name="globe"  size={20} />
+          <TextInput
+            placeholder="Country"
+            placeholderTextColor="#666666"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+             
+            ]}
+          />
+        </View>
+        <View style={styles.action}>
+          <Icon name="map-marker-outline"  size={20} />
+          <TextInput
+            placeholder="City"
+            placeholderTextColor="#666666"
+            autoCorrect={false}
+            style={[
+              styles.textInput,
+              
+            ]}
+          />
+        </View>
+        <FormButton buttonTitle="Actualizar" onPress= {() => navigation.navigate('Home')} />
+      
+    </View>
+  );
 };
 
 export default PerfilScreen;
 
 const styles = StyleSheet.create({
-
-containter: {
+  container: {
     flex: 1,
-    alignItems: 'baseline',
-    justifyContent: 'center'
-},
+  },
+  commandButton: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#FF6347',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  panel: {
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    paddingTop: 20,
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
+    // shadowColor: '#000000',
+    // shadowOffset: {width: 0, height: 0},
+    // shadowRadius: 5,
+    // shadowOpacity: 0.4,
+  },
+  header: {
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#333333',
+    shadowOffset: {width: -1, height: -3},
+    shadowRadius: 2,
+    shadowOpacity: 0.4,
+    // elevation: 5,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  panelHeader: {
+    alignItems: 'center',
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#00000040',
+    marginBottom: 10,
+  },
+  panelTitle: {
+    fontSize: 27,
+    height: 35,
+  },
+  panelSubtitle: {
+    fontSize: 14,
+    color: 'gray',
+    height: 30,
+    marginBottom: 10,
+  },
+  panelButton: {
+    padding: 13,
+    borderRadius: 10,
+    backgroundColor: '#FF6347',
+    alignItems: 'center',
+    marginVertical: 7,
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
 });
