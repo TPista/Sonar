66df35d0b66387ba7d70ec25ab8c56145b4db252
TPista <pista.10@hotmail.com>
2021-09-14 20:32:43 -0300

    telefonos utiles

--- screens/TelefonosScreen.js
+++ screens/TelefonosScreen.js
@@ -1,5 +1,6 @@
 import React, {useState, useContext} from 'react';
 import { View, Text, Button, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import { Linking } from "react-native";
 
 import { useReducer } from 'react/cjs/react.production.min';
 
@@ -10,12 +11,25 @@ import { AuthContext } from '../navigation/AuthProvider';
 
 const TelefonoScreen =({navigation})  => {
 const {user, logout}=useContext (AuthContext);
+
+const handleCallPress = () =>{
+    Linking.openURL("tel:911");
+
+};
+const handlePoliceCallPress = () =>{
+    Linking.openURL("tel:101");
+
+};
+const handleBomberCallPress = () =>{
+    Linking.openURL("tel:100");
+};
 return(
 <View >
    
     <Text justifyContent='center' > Telefonos Utiles</Text>
-    <FormButton buttonTitle="Logout" onPress={()=> logout()} />
-    
+    <FormButton buttonTitle="911" onPress={handleCallPress} />
+    <FormButton buttonTitle="Policia" onPress={handlePoliceCallPress} />
+    <FormButton buttonTitle="Bomberos" onPress={handleBomberCallPress} />    
     
     
 </View>
