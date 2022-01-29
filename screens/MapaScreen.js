f51a99bd56cf9f1cc9b1f5abdb40816976eb98e5
TPista <41997717+TPista@users.noreply.github.com>
2022-01-29 12:28:20 -0300

    Show report Emmanuel

--- screens/MapaScreen.js
+++ screens/MapaScreen.js
@@ -1,13 +1,18 @@
 import React, {useState, useContext, useEffect} from 'react';
-import { View, Text, Button, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity, Alert} from 'react-native';
 import {Component} from 'react';
 import { useReducer } from 'react/cjs/react.production.min';
 import useLocationPermisson from '../hooks/useLocationPermission';
 import useUserCoordinates from '../hooks/useUserCoordinates';
-
+import  MaterialIcons  from 'react-native-vector-icons/MaterialIcons';
 import useCrimeReportChanges from '../hooks/useCrimeReportsChanges';
 
+import { AuthContext } from '../navigation/AuthProvider';
+
+
 import MapView, { Marker } from "react-native-maps";
+import { Callout } from 'react-native-maps';
+
 
 const sanitizeReportData = (rawReportData) =>{
 
@@ -20,7 +25,79 @@ const sanitizeReportData = (rawReportData) =>{
   }
 
   
-const App = () => {
+  const handleReportDelete = (report, user) =>{
+    if(report.reporterId === user.uid){
+  return  Alert.alert(
+      "Do you want to delete this report",
+      report.description,
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+        text:"Confirm Delete",
+        onPress: () => console.log("Deleting ....")
+         
+      }
+      ]  
+    );
+  
+    }
+  
+  }
+  
+  
+  
+const App = ({navigation}) => {
+  const {user, logout}=useContext (AuthContext);
+  const [userData, setUserData]= useState (null);
+
+  const  ReportButton = ({onPress})=>{
+
+
+    const styles = StyleSheet.create({container:{
+      width:40,
+      height:40,
+      borderRadius:40/2,
+      backgroundColor: "black",
+      justifyContent:"center",
+      alignItems:"center"
+    }})
+    
+      return (
+    
+    <TouchableOpacity style={styles.container} onPress={onPress && onPress}>
+  <MaterialIcons name="report" size={24} color="white" />
+    </TouchableOpacity>
+    
+      )
+    }
+
+
+    const  DeleteButton = ({onPress})=>{
+
+
+      const styles = StyleSheet.create({container:{
+        width:40,
+        height:40,
+        borderRadius:40/2,
+        backgroundColor: "black",
+        justifyContent:"center",
+        alignItems:"center"
+      }})
+      
+        return (
+      
+      <TouchableOpacity style={styles.container} onPress={onPress && onPress}>
+    <MaterialIcons name="report" size={24} color="white" />
+      </TouchableOpacity>
+      
+        )
+      }
+
+
+
   const newReport = useCrimeReportChanges()
 
   const [marker, setMarker] = useState([]);
@@ -31,41 +108,66 @@ const App = () => {
   useEffect(()=>{ setReports(sanitizeReportData([...reports,newReport]))
   
   },[newReport.tipoDelito])
-  
+  console.log(reports)
 
 
   const handleNewMarker = (coordinate) => {
     setMarker([...marker, coordinate]);
   };
 
+
+
+
   return (
+    
     <View style={styles.container}>
+
+      <View style={{position:"absolute", right:20,bottom:40, zIndex: 1000}}>
+<ReportButton onPress= {() => navigation.navigate('Nuevo Delito')}/>
+
+</View>
       <MapView
         onPress={(e) => handleNewMarker(e.nativeEvent.coordinate)}
         style={styles.map}
         initialRegion={{
-          latitude: 37.42597730214824,
-          longitude: -122.0856026405,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: -34.6063671724,
+          longitude: -58.4357643127,
+          latitudeDelta: 0.3,
+          longitudeDelta: 0.3,
         }}
         showsUserLocation
         loadingEnabled
         mapType="terrain"
       >
+        
          {reports.length > 0 &&
           reports.map((report) => {
 
             const coordinate = report.coordinates
+           
+              const markerCoordinate = {
+              latitude:coordinate.latitude,
+              longitude:coordinate.longitude
+}
+            
 
 
             return (
-              <Marker coordinate={coordinate} key={Math.random().toString()} />
+              <Marker coordinate={markerCoordinate} key={Math.random().toString()} >
+                <Callout onPress = {({}) => handleReportDelete(report,user)}>
+
+                    <Text> { report.tipoDelito}</Text>
+                    <Text> { report.descripcion}</Text>
+                  
+                </Callout>
+
+              </Marker>
             );
           })}
-
       </MapView>
     </View>
+    
+    
   );
 };
 
